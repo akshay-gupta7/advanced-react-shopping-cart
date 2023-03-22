8d564d0175ef0d60d86f0fcc4670c779307e8f14
Akshay Gupta <gravity.akshay@gmail.com>
2023-03-22 22:49:43 +0530

    Add routes and initial component names

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Container } from "react-bootstrap"
+import { Route, Routes } from "react-router-dom"
+
 function App() {
-  return <h1>Hi</h1>
+  return <Container>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+    </Routes> 
+  </Container>
 }
 
 export default App
