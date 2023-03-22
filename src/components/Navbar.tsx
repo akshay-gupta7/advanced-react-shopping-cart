b2badacaf50d5778e57aff5c5afb074e8de4d3a5
Akshay Gupta <gravity.akshay@gmail.com>
2023-03-22 23:52:51 +0530

    Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Container, Navbar as NavbarStrap } from "react-bootstrap"
+
+export function Navbar(){
+    return <NavbarStrap className="bg-white shadow-sm mb-3">
+        <Container>
+            Nav
+        </Container>
+    </NavbarStrap>
+}
