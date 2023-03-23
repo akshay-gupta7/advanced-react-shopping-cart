88ccd915ab75fdf177cd95c8abdc5552e26e1681
Akshay Gupta <gravity.akshay@gmail.com>
2023-03-24 02:06:11 +0530

    Add shopping cart component

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
 
 type ShoppingCartContext = {
   openCart: () => void;
@@ -94,6 +95,7 @@ export function ShoppingCartProvider({ children }: ShoppingCardProviderProps) {
       }}
     >
       {children}
+      <ShoppingCart />
     </ShoppingCartContext.Provider>
   );
 }
